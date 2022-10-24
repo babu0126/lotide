ae0158bac094e125b991d6285174a78c57bbabe0
babu0126 <18davidwang@gmail.com>
2022-10-24 16:34:06 -0700

    Adding the ability to resolve the nested array

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,24 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
-  let result = true;
   if (array1.length !== array2.length) {
     return false;
-  }
-
+  } 
   for(let i = 0; i < array1.length; i ++) {
-    if (array1[i] === array2[i]) {
-      result = true;
+    if(Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      const result = eqArrays(array1[i], array2[i]);
+      if (!result) return false;
     } else {
-      result = false;
-      break;
+      if (array1[i] !== array2[i]) return false;
+      }
     }
-  }
-  return result;
-}
+    return true;
+  };
+  
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+console.log(eqArrays([2,3],[2,4])); // false
+console.log(eqArrays([2,3],[2,3])); // true
+console.log(eqArrays([[2,3],4],[[2,4],5])); // false
+console.log(eqArrays([[2,3],4],[[2,3],4])); // true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4,5]], [[2, 3], [4, 5]])); // true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
 
-// TEST CODE
-assertEqual(eqArrays([1,2,3],[1,2,3]), true);
